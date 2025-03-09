a35d6d22385de13d07c60dbc816fd5332cea9843
Yuri Blinnikov <blinnikovu5@gmail.com>
2025-03-09 22:15:06 +0800

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabContainers = document.querySelectorAll('.tab__navigation');
+
+
+    tabContainers.forEach(container => {
+        const tabs = container.querySelectorAll('.tab');
+        const contents = container.nextElementSibling.querySelectorAll('.tab__content');
+
+
+        tabs.forEach((tab, index) => {
+            tab.addEventListener('click', () => {
+
+                tabs.forEach(t => t.classList.remove('tab_active'));
+                contents.forEach(c => c.classList.remove('tab__content_active'));
+
+
+                tab.classList.add('tab_active');
+                contents[index].classList.add('tab__content_active');
+            });
+        });
+    });
+});
